57d8121429860aa8c501eeed14d142b7a4c32e62
Ryans-Developers <raynsswdevelopers@gmail.com>
2023-04-01 15:33:03 +0300

    first commit

---
+++ schemas/index.ts
@@ -0,0 +1,19 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import about from './about'
+import purpose from './purpose'
+import gallery from './gallery'
+import events from './events'
+import homeImage from './homeImage'
+import aboutImage from './aboutImage'
+import purposeImage from './purposeImage'
+import donateImage from './donateImage'
+import blogImage from './blogImage'
+import galleryImage from './galleryImage'
+import eventsImage from './eventsImage'
+import contactImage from './contactImage'
+
+
+export const schemaTypes = [post, author, category, blockContent, about, purpose, gallery, events, homeImage, aboutImage, purposeImage, donateImage, blogImage, galleryImage, eventsImage, contactImage]
